1e8fa3abc6a75ef7643909b0eebe58d5f5f4fb9e
Moskize91 <xiangtantaozeyu@gmail.com>
2017-02-09 14:05:44 +0800

    add immutable.

--- src/foundation.ts
+++ src/foundation.ts
@@ -5,3 +5,23 @@
 export type ReadOnly<T> = {
     readonly [P in keyof T]: T[P];
 };
+
+export function immutable<T>(target: T): ReadOnly<T> {
+    if (typeof target !== "object") {
+        return target;
+    }
+    if (target.constructor !== Object && target.constructor !== Function) {
+        throw new Error(`could only make a plaint pure object readonly. but the object'type is ${target.constructor}`);
+    }
+    for (const key in target) {
+        if (!Object.getOwnPropertyDescriptor(target, key).writable) {
+            Object.defineProperty(target, key, {
+                writable: false,
+                enumerable: true,
+                configurable: true,
+            });
+            immutable(target[key]);
+        }
+    }
+    return target;
+}
